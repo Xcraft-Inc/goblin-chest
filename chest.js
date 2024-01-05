060f0693ca5823c50bf7562a7c0f912a6643bb30
Mathieu Schroeter <mathieu@schroetersa.ch>
2024-01-05 14:50:15 +0100

    Rename service.js to chest.js

--- chest.js
+++ chest.js
@@ -1,4 +1,4 @@
 const {Elf} = require('xcraft-core-goblin');
-const {Chest, ChestLogic} = require('./lib/service.js');
+const {Chest, ChestLogic} = require('./lib/chest.js');
 
 exports.xcraftCommands = Elf.birth(Chest, ChestLogic);
